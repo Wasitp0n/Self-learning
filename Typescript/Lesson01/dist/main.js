d54fd0c772e19bed2c9ef44b3b5fd3302b79ce35
Wasitpon Techarungroj <131182716+Wasitp0n@users.noreply.github.com>
2023-06-18 14:42:07 +0700

    Lesson1

---
+++ Typescript/Lesson01/dist/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let username = 'Tim';
+console.log(username);
+let age = 29;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=main.js.map
---
+++ Typescript/Lesson01/src/main.ts
@@ -0,0 +1,10 @@
+let username : string = 'Tim';
+console.log(username);
+
+let age: number = 29;
+if (age < 50) 
+age+=10;
+
+console.log(age)
+
+
